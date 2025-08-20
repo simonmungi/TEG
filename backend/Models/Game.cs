9c70799fbfe5f308e5d6e42db9d11f155da94439
simonmungi <simonmungi@gmail.com>
2025-08-20 10:22:18 -0300

    ignore

--- backend/Models/Game.cs
+++ backend/Models/Game.cs
@@ -1,12 +1,21 @@
-namespace backend.Models{
+namespace backend.Models
+{
     public class Game
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public List<Player> Players { get; set; } = new List<Player>();
-        public Dictionary<string, Territory> Territories { get; set; }  = new Dictionary<string, Territory>();
+        public Dictionary<string, Territory> Territories { get; set; } = new Dictionary<string, Territory>();
         public string? CurrentPlayerId { get; set; }
         public GamePhase CurrentPhase { get; set; } = GamePhase.WaitingForPlayers;
         public List<string> TurnOrder { get; set; } = new List<string>();
         public int PendingReinforcements { get; set; }
+    }
+    
+    public enum GamePhase{
+        WaitingForPlayers,
+        Reinforcement,
+        Attack,
+        Fortification,
+        GameOver
     }
 }
